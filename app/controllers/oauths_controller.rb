cfee08a5c4968038f06d4b42c7297218c01d1639
ayuoh <aohki.0325@gmail.com>
2023-12-24 00:09:19 +0900

    update: ログインUIを調整

--- app/controllers/oauths_controller.rb
+++ app/controllers/oauths_controller.rb
@@ -10,16 +10,16 @@ class OauthsController < ApplicationController
   def callback
     provider = params[:provider]
     if (@user = login_from(provider))
-      redirect_to root_path, notice: "Logged in from #{provider.titleize}!"
+      redirect_to root_path, success: 'ログインできました!'
     else
       begin
         @user = create_from(provider)
 
         reset_session # protect from session fixation attack
         auto_login(@user)
-        redirect_to root_path, notice: "Logged in from #{provider.titleize}!"
+        redirect_to root_path, success: 'ログインできました!'
       rescue StandardError
-        redirect_to root_path, alert: "Failed to login from #{provider.titleize}!"
+        redirect_to root_path, danger: 'ログインできませんでした。'
       end
     end
   end
--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -5,21 +5,17 @@ class UsersController < ApplicationController
     @user = User.new
   end
 
-  def create
-    @user = User.new(user_params)
+  # def create
+  #   @user = User.new(user_params)
 
-    def create
-      @user = User.new(user_params)
-
-      if @user.save
-        @current_user = @user
-        redirect_to root_url
-      else
-        flash.now[:danger] = '新規登録できませんでした。'
-        render :new
-      end
-    end
-  end
+  #   if @user.save
+  #     @current_user = @user
+  #     redirect_to root_url
+  #   else
+  #     flash.now[:danger] = '新規登録できませんでした。'
+  #     render :new
+  #   end
+  # end
 
   private
 
