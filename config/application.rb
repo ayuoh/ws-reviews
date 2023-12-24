8693faa4bcabdf19e3353d8852e2134dca9c2024
ayuoh <aohki.0325@gmail.com>
2023-12-24 20:44:52 +0900

    fix: DBを日本時間に設定

--- config/application.rb
+++ config/application.rb
@@ -38,6 +38,8 @@ module WsReviews
 
     # Don't generate system test files.
     config.generators.system_tests = nil
+    config.time_zone = 'Tokyo'
+    config.i18n.default_locale = :ja
   end
 
   class ActiveRecord::Base
