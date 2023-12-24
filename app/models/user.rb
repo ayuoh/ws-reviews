851dea24e64e315e7c4424d107e6dd19735b9b46
ayuoh <aohki.0325@gmail.com>
2023-12-24 19:20:22 +0900

    fix: userモデルの修正

--- app/models/user.rb
+++ app/models/user.rb
@@ -9,12 +9,8 @@ class User < ApplicationRecord
   has_many :favorite_reviews, through: :favorites, source: :review
 
   validates :name, presence: true, length: { maximum: 50 }
-  validates :email, uniqueness: true
-  validates :twitter, uniqueness: true
-  validates :password, length: { minimum: 4 }, if: -> { new_record? || changes[:crypted_password] }
-  validates :password, confirmation: true, if: -> { new_record? || changes[:crypted_password] }
-  validates :password_confirmation, presence: true, if: -> { new_record? || changes[:crypted_password] }
-  validates :reset_password_token, uniqueness: true, allow_nil: true
+  validates :twitter_name, presence: true, uniqueness: true
+  validates :twitter_id, presence: true, uniqueness: true
 
   def own?(object)
     self.id == object.user_id
