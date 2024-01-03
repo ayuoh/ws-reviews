c82a83e25e1f54c0cc9ed8693cb653218851c963
ayuoh <aohki.0325@gmail.com>
2024-01-03 23:21:48 +0900

    add: review投稿機能を追加

--- Gemfile
+++ Gemfile
@@ -40,9 +40,30 @@ gem "tzinfo-data", platforms: %i[ windows jruby ]
 
 # Reduces boot times through caching; required in config/boot.rb
 gem "bootsnap", require: false
+gem "tailwindcss-rails", "~> 2.0"
 
 # Use Active Storage variants [https://guides.rubyonrails.org/active_storage_overview.html#transforming-images]
 # gem "image_processing", "~> 1.2"
+
+# Authentication
+gem 'sorcery'
+
+# Configuration
+gem 'config'
+gem 'figaro'
+
+# UI/UX
+gem 'rails-i18n', '~> 7.0.0'
+
+# Model
+gem 'exnum'
+
+# URL
+gem 'httparty'
+gem 'addressable'
+
+# ogp
+gem "opengraph_parser"
 
 group :development, :test do
   # See https://guides.rubyonrails.org/debugging_rails_applications.html#debugging-with-the-debug-gem
@@ -66,11 +87,3 @@ group :development do
   gem "rubocop-rails", require: false
   gem "rubocop-rspec", require: false
 end
-
-gem "tailwindcss-rails", "~> 2.0"
-
-gem 'sorcery'
-gem 'figaro'
-gem 'config'
-
-gem 'rails-i18n', '~> 7.0.0'
--- app/controllers/reviews_controller.rb
+++ app/controllers/reviews_controller.rb
@@ -5,7 +5,43 @@ class ReviewsController < ApplicationController
 
   def show; end
 
-  def new; end
+  def new
+    @review = Review.new
+  end
+
+  def create
+    @review = current_user.reviews.new(review_params)
+
+    organizer = Organizer.find_or_save_organizer(url_param)
+    web_page = organizer.web_pages.find_or_create_by(url: url_param)
+    @review.web_page_id = web_page.id
+
+    @review.set_default_title(url_param) if @review.title.blank?
+
+    if @review.save && @review.save_tag(tag_params)
+      redirect_to reviews_path, success: '投稿しました。'
+    else
+      flash.now[:danger] = '投稿できませんでした。'
+      @url = url_param
+      @tags = params.require(:review)[:tag_names]
+      render :new, status: :unprocessable_entity
+    end
+  end
 
   def edit; end
+
+  private
+
+  def review_params
+    params.require(:review).permit(:title, :content, :implementation_start_date, :implementation_last_date, :recruitment_start_date, :recruitment_last_date, :prefecture, :fee, :target)
+  end
+
+  def url_param
+    params.require(:review).permit(:url)[:url]
+  end
+
+  def tag_params
+    tags = params.require(:review)[:tag_names].split(' ')
+    tags.map!(&:strip)
+  end
 end
