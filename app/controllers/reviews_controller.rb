e1445c961a77c9660b07b89bbaf4623c50f74ed3
ayuoh <aohki.0325@gmail.com>
2024-01-09 00:45:43 +0900

    add: お気に入りのタグからのレコメンド機能実装

--- app/controllers/reviews_controller.rb
+++ app/controllers/reviews_controller.rb
@@ -17,10 +17,7 @@ class ReviewsController < ApplicationController
 
   def create
     @review = current_user.reviews.new(review_params)
-
-    organizer = Organizer.find_or_save_organizer(url_param)
-    web_page = organizer.web_pages.find_or_create_by(url: url_param)
-    @review.web_page_id = web_page.id
+    set_organizer_and_webpage
 
     @review.set_default_title(url_param) if @review.title.blank?
 
@@ -41,9 +38,7 @@ class ReviewsController < ApplicationController
 
   def update
     update_params = review_params
-    organizer = Organizer.find_or_save_organizer(url_param)
-    web_page = organizer.web_pages.find_or_create_by(url: url_param)
-    @review.web_page_id = web_page.id
+    set_organizer_and_webpage
 
     if @review.title.blank?
       @review.set_default_title(url_param)
@@ -73,6 +68,8 @@ class ReviewsController < ApplicationController
   def favorites
     @q = current_user.favorite_reviews.ransack(params[:q])
     @reviews = @q.result(distinct: true).includes(:user).order(created_at: :desc).page params[:page]
+
+    recommended_review
   end
 
   def own_reviews
@@ -97,5 +94,36 @@ class ReviewsController < ApplicationController
 
   def set_review
     @review = current_user.reviews.find(params[:id])
+  end
+
+  def set_organizer_and_webpage
+    organizer = Organizer.find_or_save_organizer(url_param)
+    web_page = organizer.web_pages.find_or_create_by(url: url_param)
+    @review.web_page_id = web_page.id
+  end
+
+  def recommended_review
+    favorite = Favorite.where(user_id: current_user.id).order(created_at: :desc).first
+    return unless favorite.present?
+
+    # 最近お気に入りにタグがあるか確認
+    fav_taggnings = Tagging.where(review_id: favorite.review_id)
+    return unless fav_taggnings.present?
+
+    # 最近お気に入りにした投稿のタグをとってくる
+    fav_tags = []
+    fav_taggnings.each do |ft|
+      fav_tags << Tag.joins(taggings: :review).where(id: ft.tag_id).distinct.to_a
+    end
+    return unless fav_tags.present?
+
+    # 検索したいtag_idを配列に格納する
+    fav_tag_ids = []
+    fav_tags.each { |t| fav_tag_ids << t[0] }
+
+    # おすすめしたいレビューを取得
+    pre_recommended_review = Review.joins(:taggings).where(taggings: { tag_id: fav_tag_ids }).where.not(reviews: { user_id: current_user.id })
+
+    @recommended_review = pre_recommended_review.where.not(id: favorite.review_id).order(created_at: :desc).first
   end
 end
