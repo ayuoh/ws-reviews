02661df5f540b8be13672b4aa222835270088743
ayuoh <aohki.0325@gmail.com>
2024-01-13 15:41:53 +0900

    add: 利用規約とプライバシーポリシーを追加

--- config/routes.rb
+++ config/routes.rb
@@ -8,6 +8,9 @@ Rails.application.routes.draw do
   # Defines the root path route ("/")
   root "reviews#index"
 
+  get 'terms', to: 'static_pages#terms', as: :terms
+  get 'privacy', to: 'static_pages#privacy', as: :privacy
+
   # get 'login', to: 'user_sessions#new', as: :login
   # post 'login', to: 'user_sessions#create'
   delete 'logout', to: 'user_sessions#destroy', as: :logout
