f1f30865fdfe642c7789c2536c3d6b45a295b180
ayuoh <aohki.0325@gmail.com>
2024-02-03 20:45:48 +0900

    update: 一覧表示と詳細表示を変更

--- app/decorators/review_decorator.rb
+++ app/decorators/review_decorator.rb
@@ -2,15 +2,19 @@
 
 module ReviewDecorator
   def implementation_tarm
-    "#{implementation_start_date.strftime('%Y/%m/%d')}〜#{implementation_last_date.strftime('%Y/%m/%d')}"
+    "#{implementation_start_date.strftime('%Y/%m/%d')} 〜 #{implementation_last_date.strftime('%Y/%m/%d')}"
   end
 
   def format_created_at
     "#{created_at.strftime('%Y/%m/%d')} 投稿"
   end
 
+  def format_prefecture
+    prefecture.present? ? prefecture : '-'
+  end
+
   def format_fee
-    return if fee.blank?
+    return '-' if fee.blank?
 
     "#{number_to_currency(fee, unit: '')}#{t('currency.unit')}"
   end
