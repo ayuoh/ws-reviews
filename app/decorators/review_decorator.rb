3d7127eacb3361bc702c8ec8b27caa3a5d1c8883
ayuoh <aohki.0325@gmail.com>
2024-02-04 17:38:39 +0900

    fix: 満足度の見た目調整

--- app/decorators/review_decorator.rb
+++ app/decorators/review_decorator.rb
@@ -18,4 +18,13 @@ module ReviewDecorator
 
     "#{number_to_currency(fee, unit: '')}#{t('currency.unit')}"
   end
+
+  def evaluate_satisfaction
+    return '高い！' if satisfaction >= 80
+    return 'やや高い' if satisfaction >= 60
+    return '普通' if satisfaction >= 40
+    return 'やや低い' if satisfaction >= 20
+
+    '低い…'
+  end
 end
